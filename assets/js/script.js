6f6819d043e01de9bcc270e3247da9a055ecaa6e
Alya Zagatskaya <alya.zagacki@gmail.com>
2022-04-22 11:15:33 +0300

    220422_1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,31 @@
 'use strict';
 
+const btn = document.querySelector('.new-msg__btn');
+btn.addEventListener('click', sendMsg);
+
+function sendMsg() {
+	printMsg(checkSpam());
+}
+
+function checkSpam() {
+	const spamExamples = [/viagra/ig, /xxx/ig];
+	let filteredMsg = '';
+	let resultText = getUserMsg();
+	for (let spamWord of spamExamples) {
+		filteredMsg = resultText.replace(spamWord, '***');
+		resultText = filteredMsg;
+	}
+	return filteredMsg;
+}
+
+function getUserMsg() {
+	return document.querySelector('.new-msg__text').value;
+}
+
+function printMsg(filteredMsg) {
+	getOutgoingBox().innerText = filteredMsg;
+}
+
+function getOutgoingBox() {
+	return document.querySelector('.message--outgoing');
+}
